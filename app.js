b4fe8c3b87539d197ef18bcf0a5c78f443242632
Danushka2 <dhanushkanuwan24@gmail.com>
2020-01-22 10:29:50 +0530

    added middlewares

--- app.js
+++ app.js
@@ -9,27 +9,112 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 
-// var sess = {
-//   secret: 'Fuck politics',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true
-// }
+var sess = {
+  secret: 'Fuck politics',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+}
 
-// if (app.get('env') === 'production') {
-//   // Use secure cookies in production (requires SSL/TLS)
-//   sess.cookie.secure = true;
+if (app.get('env') === 'production') {
+  // Use secure cookies in production (requires SSL/TLS)
+  sess.cookie.secure = true;
 
-//   // Uncomment the line below if your application is behind a proxy (like on Heroku)
-//   // or if you're encountering the error message:
-//   // "Unable to verify authorization request state"
-//   // app.set('trust proxy', 1);
-// }
+  // Uncomment the line below if your application is behind a proxy (like on Heroku)
+  // or if you're encountering the error message:
+  // "Unable to verify authorization request state"
+  // app.set('trust proxy', 1);
+}
 
-// app.use(session(sess));
+app.use(session(sess));
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------
 
 
 app.use((req, res, next) => {
