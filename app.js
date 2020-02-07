0315e37d104b91b726ab2768c28b52f77acaab18
Danushka2 <dhanushkanuwan24@gmail.com>
2020-02-08 01:52:01 +0530

    added google auth

--- app.js
+++ app.js
@@ -1,21 +1,81 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
 const db = require("./firebase-config");
 const path = require('path');
 const app = express();
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-let username;
+var username;
+
+dotenv.config();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+// config express-session
+var sess = {
+  secret: 'fuck it',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
 
-// index page 
-app.get('/x', function(req, res) {
-    res.render('index');
+if (app.get('env') === 'production') {
+  sess.cookie.secure = true; // serve secure cookies, requires https
+}
+
+app.use(session(sess));
+//----------------------------------------------------------------------
+passport.serializeUser(function (user, done) {
+  // done(null, user.id);
+  done(null, user);
 });
+
+passport.deserializeUser(function (obj, done) {
+  // Users.findById(obj, done);
+  done(null, obj);
+});
+//------------------------------------------------------------------------
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+},
+  function (accessToken, refreshToken, profile, done) {
+    console.log(profile.displayName);
+    console.log(profile.emails[0].value);
+    console.log(profile.id);
+
+    let dbDoc = profile.emails[0].value;
+    let docRef = db.collection('users').doc(dbDoc);
+
+    let getDoc = docRef.get()
+      .then(user => {
+        if (!user.exists) {
+          console.log('No such document!');
+          return done(null, user);
+        } else {
+          if (user.data().password == profile.id) {
+            console.log("Password matched");
+            username = user.data().name;
+            return done(null, user);
+          }
+        }
+      })
+      .catch(err => {
+        console.log('Error getting document', err);
+      });
+
+      return done(null, profile);
+  }
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //-------------------------------------------------------------------------------------
 
@@ -33,14 +93,31 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.render('index');
 });
 
-app.get('/dashboard', function(req, res) {
-  res.render('dashboard', {user:username});
+app.get('/dashboard', ensureAuthenticated, function (req, res) {
+  console.log(username);
+
+  if(username==undefined){
+    res.redirect('/');
+  }else{
+    res.render('dashboard', { user: username });
+  }
+
 });
- 
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['openid email profile']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google', {
+  failureRedirect: '/'
+}),
+  function (req, res) {
+    res.redirect('/dashboard');
+  });
 
 app.get('/users', (req, res) => {
   let users = [];
@@ -87,143 +164,35 @@ app.post('/login', function (req, res) {
   let docRef = db.collection('users').doc(dbDoc);
 
   let getDoc = docRef.get()
-  .then(user => {
-    if (!user.exists) {
-      console.log('No such document!');
-    } else {
-      if(req.body.passwordL == user.data().password){
-        console.log("Password matched");
-        username = user.data().name;
-        res.redirect('/dashboard');
+    .then(user => {
+      if (!user.exists) {
+        console.log('No such document!');
+      } else {
+        if (req.body.passwordL == user.data().password) {
+          console.log("Password matched");
+          username = user.data().name;
+          res.redirect('/dashboard');
+        }
       }
-    }
-  })
-  .catch(err => {
-    console.log('Error getting document', err);
-  });
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+    });
 
 });
 
 
 
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
+
 module.exports = app;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//-----------------------------------------------------------------------------
-
-// db.collection('users').get()
-//   .then((snapshot) => {
-//     snapshot.forEach((doc) => {
-//       console.log(doc.id, '=>', doc.data());
-//     });
-//   })
-//   .catch((err) => {
-//     console.log('Error getting documents', err);
-//   });
-
-
-
-// db.collection('users').get()
-//   .then((snapshot) => {
-//     snapshot.forEach((doc) => {
-//       console.log(doc.id, '=>', doc.data());
-//     });
-//   })
-//   .catch((err) => {
-//     console.log('Error getting documents', err);
-//   });
-
-
-
-
-//add new document without a generated ID
-
-// let dbDoc = 'PersonX';
-// let docRef = db.collection('users').doc(dbDoc);
-// //let docRef = db.doc("users/Dan2")
-
-// let setAda = docRef.set({
-//   first: 'Danushka',
-//   last: 'Nuwan',
-//   born: 0000
-// }).then(function () {
-//   console.log("saved");
-// }).catch(function (error) {
-//   console.log(error);
-// });
-
-
-
-// Add a new document with a generated id.  
-
-// let addDoc = db.collection('users').add({
-//   name: 'Tokyo',
-//   country: 'Japan'
-// }).then(ref => {
-//   console.log('Added document with ID: ', ref.id);
-// });
-
-
-//get data using queries
-// let usersRef = db.collection('users');
-// let query = usersRef.where('name', '==', 'Tokyo').get()
-//   .then(snapshot => {
-//     if (snapshot.empty) {
-//       console.log('No matching documents.');
-//       return;
-//     }
-
-//     snapshot.forEach(doc => {
-//       console.log(doc.id, '=>', doc.data());
-//     });
-//   })
-//   .catch(err => {
-//     console.log('Error getting documents', err);
-//   });
 
 
 
